61299bf7de6091b1417ae965a88bec2424e3ca0a
isaac montilla <montillaisaac9@gmail.com>
2025-06-20 11:36:02 -0400

    [ADD] users

--- src/authentication/dto/change-password.dto.ts
+++ src/authentication/dto/change-password.dto.ts
@@ -1,13 +1,6 @@
 // change-password.dto.ts
 import { ApiProperty } from '@nestjs/swagger';
-import { Role } from '@prisma/client';
-import {
-  IsEmail,
-  IsEnum,
-  IsNotEmpty,
-  IsString,
-  MinLength,
-} from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ChangePasswordDto {
   @ApiProperty({ description: 'Correo del usuario' })
